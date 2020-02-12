96fe2691b7ec8c6278a9dd20f043cc363b54c63a
Zzema <pzemskov@gmail.com>
2020-02-12 22:45:12 +0300

    changed hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -1,22 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func change(x *int, y *int) {
-	*x, *y = *y, *x
-
-}
 func main() {
-	x := 1
-	y := 2
-	fmt.Println(x, y) // x всё еще равен 5
-	change(&x, &y)
-	fmt.Println(x, y)
-	var buf []int
-	buf = append(buf, 9, 10)
-	buf[0] = 25
-	newbuf := buf[:]
-	newbuf[0] = 1
-	fmt.Println(buf[0])
-
+	fmt.Println("Hello world")
 }
